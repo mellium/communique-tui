8443e14098ea980f712ed9952ea41dd23bee7061
Sam Whited <sam@samwhited.com>
2018-08-02 20:42:41 -0500

    internal/roster: add multi-key shortcut system

--- internal/roster/roster.go
+++ internal/roster/roster.go
@@ -6,6 +6,10 @@
 package roster
 
 import (
+	"bytes"
+	"strconv"
+	"sync"
+
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
@@ -38,15 +42,43 @@ func New(opts ...Option) Roster {
 		list: tview.NewList(),
 	}
 	r.list.SetBorder(true)
+
+	events := &bytes.Buffer{}
+	m := &sync.Mutex{}
 	r.list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() != tcell.KeyRune {
 			return event
 		}
 
+		m.Lock()
+		defer m.Unlock()
+		events.WriteRune(event.Rune())
+
+		// TODO: this is not going to be very maintainable. Figure out a better way
+		// to handle keyboard shortcuts.
 		switch event.Rune() {
 		case 'i':
+			events.Reset()
 			return tcell.NewEventKey(tcell.KeyCR, 0, tcell.ModNone)
 		case 'k':
+			if events.Len() > 1 {
+				n, err := strconv.Atoi(events.String()[0 : events.Len()-1])
+				if err == nil {
+					n = r.list.GetCurrentItem() - n
+					if m := r.list.GetItemCount() - 1; n > m {
+						n = m
+					}
+					if n < 0 {
+						n = 0
+					}
+					r.list.SetCurrentItem(n)
+				}
+
+				events.Reset()
+				return nil
+			}
+
+			events.Reset()
 			cur := r.list.GetCurrentItem()
 			if cur <= 0 {
 				return event
@@ -54,11 +86,42 @@ func New(opts ...Option) Roster {
 			r.list.SetCurrentItem(cur - 1)
 			return nil
 		case 'j':
+			if events.Len() > 1 {
+				n, err := strconv.Atoi(events.String()[0 : events.Len()-1])
+				if err == nil {
+					n = r.list.GetCurrentItem() + n
+					if m := r.list.GetItemCount() - 1; n > m {
+						n = m
+					}
+					if n < 0 {
+						n = 0
+					}
+					r.list.SetCurrentItem(n)
+				}
+
+				events.Reset()
+				return nil
+			}
+
+			events.Reset()
 			cur := r.list.GetCurrentItem()
 			if cur >= r.list.GetItemCount()-1 {
 				return event
 			}
 			r.list.SetCurrentItem(cur + 1)
+			return nil
+		case 'G':
+			events.Reset()
+			r.list.SetCurrentItem(r.list.GetItemCount() - 1)
+			return nil
+
+		case 'g':
+			if events.String() == "gg" {
+				events.Reset()
+				r.list.SetCurrentItem(0)
+			}
+			return nil
+		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
 			return nil
 		}
 
