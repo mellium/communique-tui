d43eb8e8fcaca40381266345aec7b88cc97e7b06
Sam Whited <sam@samwhited.com>
2019-08-17 14:17:46 -0500

    internal/client: add package level doc comment

---
+++ internal/client/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
+// Package client is a high-level XMPP client implementation.
+package client
